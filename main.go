b4ad8a78fb6b6befb2c6ef5ffb342b66219131d9
Marcos Pereira Jr <marcos.pereira@mercadolivre.com>
2024-03-02 22:32:36 -0300

    Sample project 0.2.0

--- main.go
+++ main.go
@@ -9,17 +9,17 @@ const portNum string = ":8080"
 
 func main() {
 	log.Println("Starting http server.")
-	log.Println("Started on port", portNum)
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/info", info)
 
 	srv := &http.Server{
-		Addr:    ":8080",
+		Addr:    portNum,
 		Handler: mux,
 	}
 
+	log.Println("Started on port", portNum)
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Println("Fail start http server.")
